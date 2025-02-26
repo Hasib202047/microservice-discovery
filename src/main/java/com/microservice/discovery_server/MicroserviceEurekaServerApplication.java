67a74cd4a8f9c37463baf85d127d5992efea77f8
hasibul.hoque <hasibul.hoque@businessaccelerate.com.bd>
2025-02-26 16:46:01 +0600

    first commit

---
+++ src/main/java/com/microservice/discovery_server/MicroserviceEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.discovery_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceEurekaServerApplication.class, args);
+	}
+
+}
